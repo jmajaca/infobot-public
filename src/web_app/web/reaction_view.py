5ceebd0b8a13b929b3b7121fb3916cc310f692b6
jmajaca <josip.majaca@fer.hr>
2020-09-07 20:47:27 +0200

    [20] Automatic reaction scan

--- src/web_app/web/reaction_view.py
+++ src/web_app/web/reaction_view.py
@@ -14,7 +14,7 @@ reaction_manager = ReactionManager(client, DataBase(), logger)
 
 @app_reaction.route('/ui/reaction', methods=['GET'])
 def get_reactions():
-    return render_template('reaction.html'), 200
+    return render_template('reaction.html', alive=reaction_manager.is_alive()), 200
 
 
 @app_reaction.route('/ui/reaction/scan', methods=['GET'])
@@ -30,5 +30,22 @@ def scan_reactions():
 @app_reaction.route('/ui/reaction/time', methods=['POST'])
 def new_reaction_scan_time():
     match = re.search('time=([0-9]{2})%3A([0-9]{2})', request.data.decode("utf-8"))
-    print(match.group(2))
+    reaction_manager.scan_hour, reaction_manager.scan_minute = int(match.group(1)), int(match.group(2))
     return Response(status=200)
+
+
+@app_reaction.route('/ui/reaction/automatic/flip', methods=['GET'])
+def manage_automatic_reaction_scan():
+    if not reaction_manager.is_alive():
+        reaction_manager.start()
+    elif reaction_manager.is_alive():
+        reaction_manager.close()
+    return Response(status=200)
+
+
+@app_reaction.route('/ui/reaction/automatic', methods=['GET'])
+def get_reaction_manager_process_status():
+    if reaction_manager.is_alive():
+        return {'status': '1'}
+    else:
+        return {'status': '0'}
