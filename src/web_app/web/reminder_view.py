276e6918125f4ba0f4e22d94b2311f72b0cd2cb6
fstetic <filip.stetic74@gmail.com>
2020-09-18 23:07:12 +0200

    [27] Changed dropdown buttons to select forms and added table filling

--- src/web_app/web/reminder_view.py
+++ src/web_app/web/reminder_view.py
@@ -7,8 +7,9 @@ from src.models.base import DataBase
 
 app_reminder = Blueprint('app_reminder', __name__, template_folder='templates')
 logger = Logger(log_path)
-# reminder_manager = ReminderManager(client, DataBase(), logger)
+reminder_manager = ReminderManager(client, DataBase(), logger)
 
 @app_reminder.route('/ui/reminder', methods=['GET'])
 def get_reminders():
-    return render_template('reminder.html'), 200
+	reminders = reminder_manager.get_all()
+	return render_template('reminder.html', reminders=reminders), 200
