a6a363e29d7b67cd900f6a40e351c5f6763a4f2d
jmajaca <josip.majaca@fer.hr>
2020-09-19 19:20:50 +0200

    [28] Moved js scripts to separate files

---
+++ src/web_app/static/js/course.js
@@ -0,0 +1,25 @@
+function delete_course(id) {
+    let response = confirm("Are you sure you want to delete this course from database?");
+    if (response) {
+        $.post("/ui/course/delete?id=" + id, function(data){},"json");
+        setTimeout(refresh_page, 500);
+    }
+}
+
+function archive(tag) {
+    let response = confirm("Are you sure you want to archive this channel?");
+    if (response) {
+        console.log(tag)
+        $.post("/ui/channel/archive?tag=" + tag.substr(1), function(data){},"json");
+        setTimeout(refresh_page, 500);
+    }
+}
+
+function unarchive(tag) {
+    let response = confirm("Are you sure you want to unarchive this channel?");
+    if (response) {
+        console.log(tag)
+        $.post("/ui/channel/unarchive?tag=" + tag.substr(1), function(data){},"json");
+        setTimeout(refresh_page, 500);
+    }
+}
