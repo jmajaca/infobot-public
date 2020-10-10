1a238a7f0b9fafbc7ab146cebe88f4e5c203b760
fstetic <filip.stetic74@gmail.com>
2020-10-11 00:10:44 +0200

    [27] Tried implementing edit and delete and failed. Added hidden column with IDs

--- src/web_app/static/js/reminder.js
+++ src/web_app/static/js/reminder.js
@@ -10,9 +10,46 @@ function getFilters(){
 }
 
 function updateTable(data){
+	for(let i=0; i<data.result; i++){
+		data.rows[i].options = $('#reminders-table').bootstrapTable('getData')[0].options
+	}
 	$('#reminders-table').bootstrapTable('load', data)
 }
 
 function resetFilters(){
 	$.post('/ui/reminder/filter', updateTable, 'json')
+}
+
+function editReminder(eventObject){
+	// get index of the row
+	let index = parseInt(eventObject.parentElement.parentElement.attributes[0].value)
+	console.log(index)
+	let row = $('#reminders-table').bootstrapTable('getRowByUniqueId', index)
+	// $('#end_date')[index].setAttribute('contentEditable', 'true')
+	// $('#timer')[index].setAttribute('contentEditable', 'true')
+	// $('#text')[index].setAttribute('contentEditable', 'true')
+	// $('#posted')[index].setAttribute('contentEditable', 'true')
+	row.options.innerHTML = "<a class=\"mx-1\" href=\"#\" onclick=\"save(this)\">Save</a> " +
+									"<a class=\"mx-1\">or</a> " +
+									"<a class=\"mx-1\" href=\"#\" onclick=\"cancel(this)\">Cancel</a>"
+}
+
+function cancel(eventObject){
+	let index = eventObject.parentElement.parentElement.attributes[0].value
+	alert("canceled editing " + index + ". row")
+	$('#options')[index].innerHTML = "<a class=\"mx-1\" href=\"#\" onclick=\"editReminder(this)\">Edit</a> " +
+								"<a class=\"mx-1\">or</a> " +
+								"<a class=\"mx-1\" href=\"#\" onclick=\"deleteReminder(this)\">Delete</a>"
+}
+
+function save(eventObject){
+	let index = eventObject.parentElement.parentElement.attributes[0].value
+	alert("saving " + index + ". row")
+	$('#options')[index].innerHTML = "<a class=\"mx-1\" href=\"#\" onclick=\"editReminder(this)\">Edit</a> " +
+								"<a class=\"mx-1\">or</a> " +
+								"<a class=\"mx-1\" href=\"#\" onclick=\"deleteReminder(this)\">Delete</a>"
+}
+
+function deleteReminder(){
+
 }
