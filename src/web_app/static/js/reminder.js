3dfe762acb1907a58121c8732f7f4032404da740
fstetic <filip.stetic74@gmail.com>
2020-10-12 22:22:10 +0200

    [27] Added column with button and options edit and delete. Implemented part of edit functionality

--- src/web_app/static/js/reminder.js
+++ src/web_app/static/js/reminder.js
@@ -14,6 +14,31 @@ function updateTable(data){
 		data.rows[i].options = $('#reminders-table').bootstrapTable('getData')[0].options
 	}
 	$('#reminders-table').bootstrapTable('load', data)
+	// set ids for each column
+	// make 4th column a dropdown button
+	$("table#reminders-table tr").each(function() {
+		$("td", this).each(function(j) {
+			switch(j){
+				case 0:
+					this.setAttribute('id', 'end_date')
+					break;
+				case 1:
+					this.setAttribute('id', 'timer')
+					break;
+				case 2:
+					this.setAttribute('id', 'text')
+					break;
+				case 3:
+					this.setAttribute('id', 'posted')
+					break;
+				case 4:
+					this.setAttribute('id', 'options')
+					this.setAttribute('class', 'btn w-100')
+					this.setAttribute('data-toggle', 'dropdown')
+					break;
+			}
+		});
+	});
 }
 
 function resetFilters(){
@@ -21,33 +46,31 @@ function resetFilters(){
 }
 
 function editReminder(eventObject){
-	// get index of the row
-	let index = parseInt(eventObject.parentElement.parentElement.attributes[0].value)
-	console.log(index)
-	let row = $('#reminders-table').bootstrapTable('getRowByUniqueId', index)
-	// $('#end_date')[index].setAttribute('contentEditable', 'true')
-	// $('#timer')[index].setAttribute('contentEditable', 'true')
-	// $('#text')[index].setAttribute('contentEditable', 'true')
-	// $('#posted')[index].setAttribute('contentEditable', 'true')
-	row.options.innerHTML = "<a class=\"mx-1\" href=\"#\" onclick=\"save(this)\">Save</a> " +
+	let index = eventObject.parentElement.parentElement.parentElement.attributes[0].value
+	$('#end_date')[index].setAttribute('contentEditable', 'true')
+	$('#timer')[index].setAttribute('contentEditable', 'true')
+	$('#text')[index].setAttribute('contentEditable', 'true')
+	$('#posted')[index].setAttribute('contentEditable', 'true')
+	$('#menu')[index].innerHTML = "<a class=\"ml-3 mr-2\" href=\"#\" onclick=\"save(this)\">Save</a> " +
 									"<a class=\"mx-1\">or</a> " +
-									"<a class=\"mx-1\" href=\"#\" onclick=\"cancel(this)\">Cancel</a>"
+									"<a class=\"mr-3 ml-2\" href=\"#\" onclick=\"cancel(this)\">Cancel</a>"
+	$('table#reminders-table tr')[parseInt(index)+1].classList.add('table-primary')
 }
 
-function cancel(eventObject){
-	let index = eventObject.parentElement.parentElement.attributes[0].value
-	alert("canceled editing " + index + ". row")
-	$('#options')[index].innerHTML = "<a class=\"mx-1\" href=\"#\" onclick=\"editReminder(this)\">Edit</a> " +
-								"<a class=\"mx-1\">or</a> " +
-								"<a class=\"mx-1\" href=\"#\" onclick=\"deleteReminder(this)\">Delete</a>"
+function cancel(eventObject) {
+	let index = eventObject.parentElement.parentElement.parentElement.attributes[0].value
+	$('#menu')[index].innerHTML = "<a class=\"ml-3 mr-2\" href=\"#\" onclick=\"editReminder(this)\">Edit</a> " +
+									"<a class=\"mx-1\">or</a> " +
+									"<a class=\"mr-3 ml-2\" href=\"#\" onclick=\"deleteReminder(this)\">Delete</a>"
+	$('table#reminders-table tr')[parseInt(index)+1].classList.remove('table-primary')
 }
 
 function save(eventObject){
-	let index = eventObject.parentElement.parentElement.attributes[0].value
-	alert("saving " + index + ". row")
-	$('#options')[index].innerHTML = "<a class=\"mx-1\" href=\"#\" onclick=\"editReminder(this)\">Edit</a> " +
+	let index = eventObject.parentElement.parentElement.parentElement.attributes[0].value
+	$('#menu')[index].innerHTML = "<a class=\"ml-3 mr-2\" href=\"#\" onclick=\"editReminder(this)\">Edit</a> " +
 								"<a class=\"mx-1\">or</a> " +
-								"<a class=\"mx-1\" href=\"#\" onclick=\"deleteReminder(this)\">Delete</a>"
+								"<a class=\"mr-3 ml-2\" href=\"#\" onclick=\"deleteReminder(this)\">Delete</a>"
+	$('table#reminders-table tr')[parseInt(index)+1].classList.remove('table-primary')
 }
 
 function deleteReminder(){
