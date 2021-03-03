b5f870df12c7c37482edd6fb007a4257258adfff
jmajaca <josip.majaca@gmail.com>
2021-03-03 15:40:03 +0100

    [46] Deployment script added

---
+++ src/resources/bash_scripts/deployment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker stop infobot-app
+docker rm infobot-app
+cd ~/git/infobot-public || exit 1
+docker build --tag infobot-app f Dockerfile .
+docker run --name infobot-app -p 9000:9000 infobot-app
+exit 0
