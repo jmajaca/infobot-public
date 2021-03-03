da5121c96b1b9d60ddf1f6f2d6c9bb3d96f0e596
jmajaca <josip.majaca@gmail.com>
2021-03-03 17:33:09 +0100

    [48] Pull on demand

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /app
 RUN pip install --upgrade pip
 RUN pip install -r src/resources/requirements.txt
 EXPOSE 9000
-CMD git pull && python start.py
+CMD python start.py
--- src/resources/bash_scripts/deployment.sh
+++ src/resources/bash_scripts/deployment.sh
@@ -3,5 +3,6 @@
 docker stop infobot-app
 docker rm infobot-app
 cd ~/git/infobot-public || exit 1
+git pull
 docker build --tag infobot-app f Dockerfile .
 docker run --name infobot-app -p 9000:9000 infobot-app | exit 0
