65208297f089d76652ab2f63fd973149077b5d5a
jmajaca <josip.majaca@gmail.com>
2021-03-06 18:09:11 +0100

    [42] Added execution rights to all on all bash scripts

--- src/resources/bash_scripts/deployment.sh
+++ src/resources/bash_scripts/deployment.sh
@@ -10,12 +10,14 @@ echo "$(info_log) Stopped infobot-app container"
 docker rm infobot-app > /dev/null 2>&1
 echo "$(info_log) Removed infobot-app container"
 cd ~/git/infobot-public || exit 1
+echo "$(info_log) Resetting git repo"
+git reset --hard
 echo "$(info_log) Pulling from git repo"
 git pull
 docker build --tag infobot-app -f Dockerfile .
 echo "$(info_log) Build new docker image finished"
 docker run --name infobot-app -p 9000:9000 infobot-app &
 echo "$(info_log) Infobot application started"
-curl GET http://207.246.120.216:9000/ui/home/ui/home/scraper/start
+curl GET "http://$DEPLOY_HOST/ui/home/ui/home/scraper/start"
 echo "$(info_log) Scraper started"
 exit 0
