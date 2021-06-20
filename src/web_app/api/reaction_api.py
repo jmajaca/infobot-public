3fe24842a4bf8eb5c54f3639fa2f908b06362fbb
jmajaca <josip.majaca@gmail.com>
2021-06-20 13:42:03 +0200

    [55] Reaction no longer plain text

--- src/web_app/api/reaction_api.py
+++ src/web_app/api/reaction_api.py
@@ -15,16 +15,17 @@ def get_reaction_table():
     database = DataBase()
     data, data_ok = SlackCommandUtils.read_data(request=request, text_tokens_length=2)
     try:
+        reaction_name = data['text'][1][1:-1]
         if not data_ok:
             return 'Invalid request format', 400
         if data['text'][0] == 'r':
-            result = reaction_manager.get_top_receivers(reaction_name=data['text'][1])
+            result = reaction_manager.get_top_receivers(reaction_name=reaction_name)
         elif data['text'][0] == 's':
-            result = reaction_manager.get_top_senders(reaction_name=data['text'][1])
+            result = reaction_manager.get_top_senders(reaction_name=reaction_name)
         else:
             return 'Invalid request format', 400
         response = {'response_type': 'in_channel'}
-        response_text = 'Here is top chart for :' + data['text'][1] + ':\n\n'
+        response_text = 'Here is top chart for :' + reaction_name + ':\n\n'
         for i, info in enumerate(result):
             response_text += str(i + 1) + '. <@' + database.select(SlackUser, name=info[0]).id + '> with ' + str(info[1]) + ' total '
             if data['text'][0] == 'r':
